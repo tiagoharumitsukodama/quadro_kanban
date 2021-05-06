771a9de02d4d707a54134a3e9db498bd546e6b1f
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-05-06 01:59:07 -0300

    deleted

--- server/models/callback_auth.js
+++
@@ -1,9 +0,0 @@
-
-module.exports.authenticate_user = async ( name, password ) => {
-    
-    if( name == process.env.USER_NAME && password == process.env.USER_PASSWORD ){
-        return name
-    }
-    else
-        return null
-}
--- server/models/config.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-    "dialect": "sqlite",
-    "storage": ":memory",
-    "logging": false
-  })
-  
-module.exports = sequelize;
